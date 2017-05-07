80278aa09165557abc823716e1e69401dcd1139a
Luna <judahiii@gmail.com>
2017-05-07 03:38:33 -0400

    Don't allow AI to build when being attacked

--- ai.js
+++ ai.js
@@ -11,27 +11,30 @@ function selectBody(team) {
 	var owned = [];
 	bodies(function(body, name) {
 		if (body.hasOwnProperty("owner") && body.owner == team) {
-			owned.push(name);
+			// Don't build on planets that are engaged in battle at this moment
+			if (!(attacking && getBody(focusedBody).owner == team)) {
+				owned.push(name);
+			}
 		}
 	});
 	return randFromList(owned);
 }
 
-function aiBuild() {
-	$.each(teams, function(name, team) {
-		if (name != "player") {
-			if (team.nextBuild) {
-				if (build(team.nextBuild, name, selectBody(name))) {
-					selectBuild(name);
-				}
-			}
-			else {
-				selectBuild(name);
-			}
+function aiBuild(name, team) {
+	if (team.nextBuild) {
+		if (build(team.nextBuild, name, selectBody(name))) {
+			selectBuild(name);
 		}
-	});
+	}
+	else {
+		selectBuild(name);
+	}
 }
 
 function ai() {
-	aiBuild();
+	$.each(teams, function(name, team) {
+		if (name != "player") {
+			aiBuild(name, team);
+		}
+	});
 }
